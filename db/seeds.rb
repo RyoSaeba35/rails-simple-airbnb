c5200622c2efe982fdd8e62673e18e5745c61282
Pierre Libran <pedro89@hotmail.fr>
2024-08-07 16:31:13 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+
+Flat.create!(
+  name: 'Super flat in Croix Rousse',
+  address: '23 rue Dumenge',
+  description: 'A lovely and cosy 3 rooms flat',
+  price_per_night: 200,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Dark studio in Italy',
+  address: '10 rue de Roma',
+  description: 'A disgusting smelly pizza flat',
+  price_per_night: 1000,
+  number_of_guests: 500
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+puts "Finished!"
